ff2bb52835b2b762bc48383a07a09af332c8e9a3
Alex Kellner <alexander.kellner@in2code.de>
2019-07-30 15:07:32 +0200

    [TASK] Add ext_emconf

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'publications',
+    'description' => 'Publication reference extension with im- and export with bibtex and xml files',
+    'category' => 'plugin',
+    'version' => '1.0.0',
+    'author' => 'Alex Kellner',
+    'author_email' => 'alexander.kellner@in2code.de',
+    'author_company' => 'in2code.de',
+    'state' => 'stable',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-9.5.99'
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ]
+];
