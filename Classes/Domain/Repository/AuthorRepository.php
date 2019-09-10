7b448de3a9dbe4c87941cf4ea4e6e683fc195f00
Alex Kellner <alexander.kellner@in2code.de>
2019-09-10 13:57:29 +0200

    [BUGFIX] Don't respect default content storage page
    This bug comes up if there is a content element with a filled tt_content.pages field

--- Classes/Domain/Repository/AuthorRepository.php
+++ Classes/Domain/Repository/AuthorRepository.php
@@ -2,11 +2,9 @@
 declare(strict_types=1);
 namespace In2code\Publications\Domain\Repository;
 
-use TYPO3\CMS\Extbase\Persistence\Repository;
-
 /**
  * Class AuthorRepository
  */
-class AuthorRepository extends Repository
+class AuthorRepository extends AbstractRepository
 {
 }
