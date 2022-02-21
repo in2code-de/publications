0444d7da2d0041c2ee47a1c37863126bdba56762
Marcus Schwemer <mschwemer@users.noreply.github.com>
2022-02-21 09:49:29 +0100

    [FEATURE] Raise TYPO3 compatibility to 11.5
    In order to start working on TYPO3 compatibility with 11.5, we need to raise the version
    number there
    
    Signed-off-by: Marcus Schwemer <marcus.schwemer@in2code.de>

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.9.99'
+            'typo3' => '11.5.0-11.5.99'
         ],
         'conflicts' => [],
         'suggests' => [],
