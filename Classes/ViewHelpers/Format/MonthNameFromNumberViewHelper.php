05c97c58679dd959ccc0091ed7c263845467a12c
kfumy <klaus.fumy@uni-ulm.de>
2022-11-25 09:53:28 +0100

    Update MonthNameFromNumberViewHelper.php

--- Classes/ViewHelpers/Format/MonthNameFromNumberViewHelper.php
+++ Classes/ViewHelpers/Format/MonthNameFromNumberViewHelper.php
@@ -31,6 +31,44 @@ class MonthNameFromNumberViewHelper extends AbstractViewHelper implements Single
     public function render(): string
     {
         $month = $this->arguments['month'];
+        $monthlower = strtolower ($month);
+        $monthletter = substr($monthlower, 0, 3);
+        if ($monthletter == 'jan') {
+            $month = '1';
+        }
+        if ($monthletter == 'feb') {
+            $month = '2';
+        }
+        if ($monthletter == 'mar' || $monthletter == 'mär') {
+            $month = '3';
+        }
+        if ($monthletter == 'apr') {
+            $month = '4';
+        }
+        if ($monthletter == 'may' || $monthletter == 'mai') {
+            $month = '5';
+        }
+        if ($monthletter == 'jun') {
+            $month = '6';
+        }
+        if ($monthletter == 'jul') {
+            $month = '7';
+        }
+        if ($monthletter == 'aug') {
+            $month = '8';
+        }
+        if ($monthletter == 'sep') {
+            $month = '9';
+        }
+        if ($monthletter == 'oct' || $monthletter == 'okt') {
+            $month = '10';
+        }
+        if ($monthletter == 'nov') {
+            $month = '11';
+        }
+        if ($monthletter == 'dec' || $monthletter == 'dez') {
+            $month = '12';
+        }
         if ($month > 0) {
             if (MathUtility::canBeInterpretedAsInteger($month)) {
                 $month = LocalizationUtility::translate('month.' . $month, 'publications');
