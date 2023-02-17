f69e27bca8a8e3dd7fa757c062566ebd0b1c4f7f
Sebastian Stein <sebastian.stein@in2code.de>
2023-02-17 09:54:59 +0100

    [TASK] add TYPO3 V12 compatibility for local ddev development setup

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'publications',
     'description' => 'Publication reference extension with im- and export with bibtex and xml files',
     'category' => 'plugin',
-    'version' => '5.2.0',
+    'version' => '6.0.0-dev',
     'author' => 'Alex Kellner',
     'author_email' => 'alexander.kellner@in2code.de',
     'author_company' => 'in2code.de',
     'state' => 'stable',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99'
+            'typo3' => '11.5.0-12.99.99'
         ],
         'conflicts' => [],
         'suggests' => [],
