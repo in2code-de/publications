61571d6aebb6d85cc6225e776226daa3680e74fe
Marcus Schwemer <marcus.schwemer@in2code.de>
2026-01-20 15:48:48 +0100

    [BUGFIX] Prevent undefined array key warnings in ImportService
    Related: https://projekte.in2code.de/issues/76539 (internal)
    Related: in2code-de/publications#80

--- Classes/Service/ImportService.php
+++ Classes/Service/ImportService.php
@@ -210,10 +210,10 @@ class ImportService extends AbstractService
 
         $currentPublication = $this->getPublicationByIdentifier(
             $this->storagePid,
-            $record['title'],
-            $record['year'],
-            $record['citeid'],
-            $record['bibtype']
+            $updatedPublication['title'] ?? '',
+            $updatedPublication['year'] ?? '',
+            $updatedPublication['citeid'] ?? '',
+            $updatedPublication['bibtype'] ?? '',
         );
 
         if (!empty($currentPublication)) {
@@ -233,10 +233,10 @@ class ImportService extends AbstractService
     {
         $currentPublication = $this->getPublicationByIdentifier(
             $this->storagePid,
-            $updatedPublication['title'],
-            $updatedPublication['year'],
-            $updatedPublication['citeid'],
-            $updatedPublication['bibtype']
+            $updatedPublication['title'] ?? '',
+            $updatedPublication['year'] ?? '',
+            $updatedPublication['citeid'] ?? '',
+            $updatedPublication['bibtype'] ?? '',
         );
 
         $fieldsToUpdate = $this->getFieldsToUpdate($currentPublication, $updatedPublication);
